a342207f27438e63a4ff99abb43b7f9edde218a3
sthgrau <sthgrau@gmail.com>
2017-10-13 18:19:39 -0400

    added editor

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var myJSON = "";
+var mykeys = {};
+var profileName = "profile";
+
 function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
     var myreturn="";
@@ -58,14 +62,146 @@ function fillFonts() {
     document.getElementById('output').style.fontFamily = fs.value;
 }
 
-var myStuff = readTextFile("stuff.json");
-var myJSON = JSON.parse(myStuff).sort(function(a,b) { return parseInt(b.level) - parseInt(a.level); } );
-var mykeys = {};
-for ( var i = 0; i < myJSON.length; i++ ) {
-    myJSON[i].found=0;
-    mykeys[myJSON[i].id] = myJSON[i];
+function getProfileKeys() {
+    myJSON = JSON.parse(document.getElementById("rawProfile").value).sort(function(a,b) { return parseInt(b.level) - parseInt(a.level); } );
+    mykeys = {};
+    for ( var i = 0; i < myJSON.length; i++ ) {
+        myJSON[i].found=0;
+        mykeys[myJSON[i].id] = myJSON[i];
+    }
 }
 
+function saveLocalProfile(loc) {
+    localStorage[profileName] = document.getElementById('rawProfile').value;
+}
+
+function loadProfile(loc) {
+    var myStuff = "";
+    if ( loc == "local" ) {
+        if ( typeof(localStorage[profileName] ) != 'undefined' ) {
+            myStuff = localStorage[profileName];
+        }
+    }
+    else if ( loc == "remote" ) {
+        myStuff = readTextFile("stuff.json");
+    }
+    if ( myStuff !== "" ) {
+        document.getElementById('rawProfile').value = myStuff;
+        var listele = document.getElementById('lineItems');
+        listele.innerHTML="";
+        myJSON = JSON.parse(myStuff).sort(function(a,b) { return parseInt(b.level) - parseInt(a.level); } );
+        var tit = document.createElement('span');
+        var tbr = document.createElement('br');
+        var idt = document.createElement('input');
+        idt.className = "eid";
+        idt.value = "Match string";
+        idt.readOnly = true;
+        idt.style = "background-color: #CCCCCC;";
+        tit.appendChild(idt);
+        var levelt = document.createElement('input');
+        levelt.className = "elevel";
+        levelt.value = "Lvl";
+        levelt.readOnly = true;
+        levelt.style = "background-color: #CCCCCC;";
+        tit.appendChild(levelt);
+        var linet = document.createElement('input');
+        linet.className = "eline";
+        linet.value = "Sentence to use in letter";
+        linet.readOnly = true;
+        linet.style = "background-color: #CCCCCC;";
+        tit.appendChild(linet);
+        var typet = document.createElement('input');
+        typet.className = "etype";
+        typet.value = "Type";
+        typet.readOnly = true;
+        typet.style = "background-color: #CCCCCC;";
+        tit.appendChild(typet);
+        tit.appendChild(tbr);
+        listele.appendChild(tit);
+        for ( var i = 0; i < myJSON.length; i++ ) {
+            myJSON[i].found=0;
+            var br = document.createElement('br');
+            var ele = document.createElement('span');
+            ele.className = 'listItem';
+            var idf = document.createElement('input');
+            idf.value = myJSON[i].id;
+            idf.className = 'eid';
+            ele.appendChild(idf);
+            var levelf = document.createElement('input');
+            levelf.value = myJSON[i].level;
+            levelf.className = 'elevel';
+            ele.appendChild(levelf);
+            var linef = document.createElement('input');
+            linef.value = myJSON[i].line.replace(/\n/g,"\\n");
+            linef.className = 'eline';
+            ele.appendChild(linef);
+            var typef = document.createElement('input');
+            typef.value = ( myJSON[i].type ) ? myJSON[i].type : "skill";
+            typef.className = 'etype';
+            ele.appendChild(typef);
+            var but = document.createElement('button');
+            but.innerText = "-";
+            but.id = "delButton";
+            ele.appendChild(but);
+            but.onclick = (function(e) { 
+                e.target.parentNode.remove();
+            });
+            ele.appendChild(br);
+            listele.appendChild(ele);
+        }
+        getProfileKeys();
+
+        var but = document.createElement('button');
+        but.innerText = "+";
+        but.id = "addButton";
+        but.onclick = (function(e) { 
+            var br = document.createElement('br');
+            var ele = document.createElement('span');
+            var idf = document.createElement('input');
+            idf.className = 'eid';
+            ele.appendChild(idf);
+            var levelf = document.createElement('input');
+            levelf.className = 'elevel';
+            ele.appendChild(levelf);
+            var linef = document.createElement('input');
+            linef.className = 'eline';
+            ele.appendChild(linef);
+            var typef = document.createElement('input');
+            typef.className = 'etype';
+            ele.appendChild(typef);
+            var but = document.createElement('button');
+            but.innerText = "-";
+            but.id = "delButton";
+            ele.appendChild(but);
+            but.onclick = (function(e) { 
+                e.target.parentNode.remove();
+            });
+            ele.appendChild(br);
+            e.target.parentNode.insertBefore(ele,e.target);
+        });
+        listele.appendChild(but);
+
+        var sav = document.createElement('button');
+        sav.innerText = "Save Local";
+        sav.id = "saveButton";
+        sav.onclick = (function(e) { 
+            var newJson = [];
+            e.target.parentNode.childNodes.forEach( function(f) { 
+                if ( f.tagName.toLowerCase() == "span" && f.className == 'listItem' ) { 
+                    var id = f.getElementsByClassName('eid')[0].value;
+                    var level = parseInt(f.getElementsByClassName('elevel')[0].value);
+                    var line = f.getElementsByClassName('eline')[0].value.replace(/\\n/g,"\n");
+                    var type = f.getElementsByClassName('etype')[0].value;
+                    newJson.push({id, level, line, type});
+                }
+            });
+            document.getElementById('rawProfile').value = JSON.stringify(newJson, null, '\t');
+            localStorage[profileName] = JSON.stringify(newJson, null, '\t');
+            getProfileKeys();
+        });
+        listele.appendChild(sav);
+    }
+}
 
 function doit() {
     var desc = " " + document.getElementById('desc').value.replace(/[.,;:,.'"\/\]\[\-=_+!@#$%^&*()`~]+/g, " ") + " ";
@@ -89,5 +225,4 @@ function doit() {
         }
     }
     document.getElementById('noprint').classList.toggle('noprint');
-    console.log( mykeys);
 }
